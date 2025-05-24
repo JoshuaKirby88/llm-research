f3b2ef895ebd5144ede0c06ef6169a6f8d3272a8
Joshua Kirby <jojokirby88@gmail.com>
2025-05-24 09:10:09 +0100

    Remove unused assets.

--- components/font.ts
+++
@@ -1,6 +0,0 @@
-import localFont from "next/font/local"
-
-export const Virgil = localFont({
-	src: "../public/Virgil.woff2",
-	display: "swap",
-})
--- drizzle/config.ts
+++ drizzle/config.ts
@@ -1,4 +1,3 @@
-import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 import { getLocalDB } from "./get-local-db"
 

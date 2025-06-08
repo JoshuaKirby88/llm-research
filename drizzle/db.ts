50cbf3bfadeeb8365eee626b7a9babf2b155e5c3
Joshua Kirby <jojokirby88@gmail.com>
2025-06-08 15:28:25 +0900

    D1 read replica with sessions.

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -3,4 +3,4 @@ import { getCloudflareContext } from "@opennextjs/cloudflare"
 import { drizzle } from "drizzle-orm/d1"
 
 const env = (await getCloudflareContext({ async: true })).env
-export const db = drizzle(env.DB, { schema })
+export const db = drizzle(env.DB.withSession() as unknown as D1Database, { schema })

0f0036165dadc4ac89b221443bc9799cd90f9612
Joshua Kirby <jojokirby88@gmail.com>
2025-06-11 06:49:43 +0900

    Removed automatic variable name update.

--- app/new/[[...researchIds]]/_components/variable-slash-editor.tsx
+++ app/new/[[...researchIds]]/_components/variable-slash-editor.tsx
@@ -2,11 +2,9 @@
 
 import { FormSlashEditor } from "@/components/form/client/form-slash-editor"
 import { Suggestion } from "@/components/slash-editor"
-import { SlashEditorFeature } from "@/src/features"
 import { CreateResearchI } from "@/src/schemas"
 import { VariableTable } from "@/src/tables"
 import { BotMessageSquareIcon, SquareChevronRightIcon, VariableIcon } from "lucide-react"
-import { useEffect, useRef } from "react"
 import { UseFieldArrayReturn, useFormContext } from "react-hook-form"
 
 const config = {
@@ -28,7 +26,6 @@ const config = {
 		label: VariableTable.messageToVarName({ type: "completion", role: "assistant", count: 1 }),
 		description: "The generated completion, based on the generated messages.",
 	} satisfies Suggestion,
-	dependentFields: ["evalPrompt.text"] as const,
 	messageTemplateIndexToCount(index: number, messageTemplateFields: UseFieldArrayReturn<CreateResearchI, "messageTemplates", "id">) {
 		const role = messageTemplateFields.fields[index].role
 		const messageOfRoleCount = messageTemplateFields.fields.slice(0, index + 1).filter(field => field.role === role).length
@@ -39,55 +36,6 @@ const config = {
 export const VariableSlashEditor = (props: { name: string; index: number; messageTemplateFields: UseFieldArrayReturn<CreateResearchI, "messageTemplates", "id"> }) => {
 	const form = useFormContext<CreateResearchI>()
 	const [independent, blockings] = form.watch(["independentVariable", "blockingVariables"])
-	const blockingNames = blockings.map(b => b.name)
-	const prevRef = useRef({ independentVariableName: independent.name, blockingVariableNames: blockingNames })
-
-	useEffect(() => {
-		// Independent variable name
-		if (prevRef.current.independentVariableName !== independent.name) {
-			form.getValues("messageTemplates").forEach((field, i) => {
-				const currentValue = SlashEditorFeature.tiptapStringToCustomString(field.text)
-				if (currentValue.includes(VariableTable.toVar(prevRef.current.independentVariableName))) {
-					const updatedValue = currentValue.replaceAll(VariableTable.toVar(prevRef.current.independentVariableName), VariableTable.toVar(independent.name))
-					props.messageTemplateFields.update(i, { ...field, text: SlashEditorFeature.customStringToTiptapString(updatedValue) })
-				}
-			})
-
-			config.dependentFields.forEach(dependentField => {
-				const currentValue = SlashEditorFeature.tiptapStringToCustomString(form.getValues(dependentField))
-				if (currentValue.includes(VariableTable.toVar(prevRef.current.independentVariableName))) {
-					const updatedValue = currentValue.replaceAll(VariableTable.toVar(prevRef.current.independentVariableName), VariableTable.toVar(independent.name))
-					form.setValue(dependentField, SlashEditorFeature.customStringToTiptapString(updatedValue))
-				}
-			})
-		}
-
-		// Blocking variable names
-		if (blockings.length === prevRef.current.blockingVariableNames.length) {
-			const updatedBlockings = prevRef.current.blockingVariableNames.map((prevName, i) => ({ prevName, i })).filter(blocking => blocking.prevName !== blockings[blocking.i].name)
-
-			updatedBlockings.forEach(updatedBlocking => {
-				form.getValues("messageTemplates").forEach((field, i) => {
-					const currentValue = SlashEditorFeature.tiptapStringToCustomString(field.text)
-					if (currentValue.includes(VariableTable.toVar(updatedBlocking.prevName))) {
-						const updatedValue = currentValue.replaceAll(VariableTable.toVar(updatedBlocking.prevName), VariableTable.toVar(blockings[updatedBlocking.i].name))
-						props.messageTemplateFields.update(i, { ...field, text: SlashEditorFeature.customStringToTiptapString(updatedValue) })
-					}
-				})
-
-				config.dependentFields.forEach(dependentField => {
-					const currentValue = SlashEditorFeature.tiptapStringToCustomString(form.getValues(dependentField))
-					if (currentValue.includes(VariableTable.toVar(updatedBlocking.prevName))) {
-						const updatedValue = currentValue.replaceAll(VariableTable.toVar(updatedBlocking.prevName), VariableTable.toVar(blockings[updatedBlocking.i].name))
-						form.setValue(dependentField, SlashEditorFeature.customStringToTiptapString(updatedValue))
-					}
-				})
-			})
-		}
-
-		prevRef.current.independentVariableName = independent.name
-		prevRef.current.blockingVariableNames = blockingNames
-	}, [independent.name, JSON.stringify(blockingNames)])
 
 	const suggestions: Suggestion[] = [
 		...(independent.name ? [config.createVariableSuggestion(independent.name, independent.values)] : []),
--- todos.md
+++ todos.md
@@ -1,18 +1,11 @@
-- [ ] Updating variable names broken
 - [ ] Fix run test loading
 - [ ] Add types to batch structured completion
 - [ ] Attempt large research with large tests, mocking batch ai, returning static text
-- [ ] Fix clerk middleware error
 - [ ] Light theme flash on prod when theme is dark (Syntax on theme had solution)
-
-# New research form
-
-- [ ] Compare mode: Each test has 2 different messages. Evaluation prompt can access both, and compare
-- [ ] Generate sample prompts for all prompt inputs
-- [ ] Support research without blocking variables
-    - Make test filter page more flexible to different data
-- [ ] Long tags get clipped. Make it scrollable
-- [ ] Ability to enter example messages to generate variables and message prompts from
+- [ ] Compare completion mode
+- [ ] Generate sample button for research create form
+- [ ] Research without blocking variables
+- [ ] Fix long tags getting clipped in research create form
 
 # Get Cloudflare Pro
 
